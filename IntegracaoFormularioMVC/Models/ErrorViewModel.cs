864dd87f569cd9355b33e7e87f1139f52132feee
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-01-11 21:28:37 -0300

    Configurando ambiente de desenvolvimento IntegraçãoFormularioMVC

---
+++ IntegracaoFormularioMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntegracaoFormularioMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
