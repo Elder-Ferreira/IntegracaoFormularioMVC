bbb1ca76565ff16598fc15b075ca0ca8ac25a268
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-01-13 18:09:01 -0300

    Criação da classe LivrosModel

---
+++ IntegracaoFormularioMVC/Models/LivrosModel.cs
@@ -0,0 +1,12 @@
+﻿namespace IntegracaoFormularioMVC.Models
+{
+    public class LivrosModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Paginas { get; set; }
+        public bool CapaDura { get; set; }
+        public DateTime DataCompra { get; set; } = DateTime.Now;
+    }
+}
--- IntegracaoFormularioMVC/obj/Debug/net6.0/IntegracaoFormularioMVC.AssemblyInfo.cs
+++ IntegracaoFormularioMVC/obj/Debug/net6.0/IntegracaoFormularioMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegracaoFormularioMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+864dd87f569cd9355b33e7e87f1139f52132feee")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegracaoFormularioMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegracaoFormularioMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
