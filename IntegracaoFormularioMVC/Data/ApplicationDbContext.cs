45f3101b4c9037e5e2edbeb34a74fdc0271bffbe
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-01-15 16:34:37 -0300

    Criado ApplicationDbContext e configurado conexão com banco de dados SQLServer

---
+++ IntegracaoFormularioMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IntegracaoFormularioMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegracaoFormularioMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<LivrosModel> Livros { get; set; }
+    }
+}
--- IntegracaoFormularioMVC/obj/Debug/net6.0/IntegracaoFormularioMVC.AssemblyInfo.cs
+++ IntegracaoFormularioMVC/obj/Debug/net6.0/IntegracaoFormularioMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegracaoFormularioMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+864dd87f569cd9355b33e7e87f1139f52132feee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de08d56e760abe45e395aef8fb81cf2c69d130ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegracaoFormularioMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegracaoFormularioMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
