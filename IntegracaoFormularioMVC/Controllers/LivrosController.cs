1299c6ecc05e723e628ad3c0cff8e08e026444c1
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-01-18 23:36:19 -0300

    Criado LivrosController e View

---
+++ IntegracaoFormularioMVC/Controllers/LivrosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntegracaoFormularioMVC.Controllers
+{
+    public class LivrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
