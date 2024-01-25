06fda86c68d7bd7fb18f16a6fc8829af2a3e5312
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-01-24 23:50:26 -0300

    Criado validação do formulario de cadastro e complemento da tabela Livros

--- IntegracaoFormularioMVC/Models/LivrosModel.cs
+++ IntegracaoFormularioMVC/Models/LivrosModel.cs
@@ -6,9 +6,13 @@ namespace IntegracaoFormularioMVC.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o título do livro!")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "Digite o nome do autor!")]
         public string Autor { get; set; }
+        [Required(ErrorMessage = "Digite o número de páginas!")]
         public int Paginas { get; set; }
+        [Required(ErrorMessage = "Informe se o livro possui capa especial!")]
         public bool CapaDura { get; set; }
         public DateTime DataCompra { get; set; } = DateTime.Now;
     }
