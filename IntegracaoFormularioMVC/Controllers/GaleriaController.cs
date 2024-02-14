ac5cacf60c01cf628d949afead977fb30fc3bf05
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-02-14 17:47:50 -0300

    Criação GaleriaController.cs

---
+++ IntegracaoFormularioMVC/Controllers/GaleriaController.cs
@@ -0,0 +1,22 @@
+﻿using IntegracaoFormularioMVC.Data;
+using IntegracaoFormularioMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegracaoFormularioMVC.Controllers
+{
+    public class GaleriaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public GaleriaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult<List<ImagemModel>>> Index()
+        {
+            var imagens = await _context.Imagens.ToListAsync();
+            return View();
+        }
+    }
+}
+
